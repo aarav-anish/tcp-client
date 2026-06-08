2a2dd1159c07a0f03c4c618c521d187c33bb8a72
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2026-06-08 18:24:43 +0000

    Connect to the server

--- sockets.c
+++ sockets.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define IP "172.217.26.35" /* www.google.se */
 #define PORT 80            /* http*/
 
 int main()
 {
+    int s;
+
+    struct sockaddr_in sock;
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (s < 0)
+    {
+        printf("Socket error");
+        return -1;
+    }
+
+    sock.sin_addr.s_addr = inet_addr(IP);
+    sock.sin_port = htons(PORT);
+    sock.sin_family = AF_INET;
+
+    if (connect(s, (struct sockaddr *)&sock, sizeof(struct sockaddr_in)) != 0)
+    {
+        printf("connect() error");
+        close(s);
+
+        return -1;
+    }
     return 0;
 }
