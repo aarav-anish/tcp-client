fcdfc8c7e789538adc81eb8af143b9419ef33900
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2026-06-08 20:11:02 +0000

    Send and receive data from the server

--- sockets.c
+++ sockets.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 #define IP "172.217.26.35" /* www.google.se */
 #define PORT 80            /* http*/
@@ -13,6 +14,10 @@ int main()
     int s;
 
     struct sockaddr_in sock;
+    char buf[512];
+
+    char *data;
+    data = "HEAD / HTTP/1.0\n\n";
 
     s = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -33,5 +38,12 @@ int main()
 
         return -1;
     }
+
+    write(s, data, strlen(data));
+    read(s, buf, 511);
+    close(s);
+
+    printf("socket data:\n\n%s\n\n", buf);
+
     return 0;
 }
